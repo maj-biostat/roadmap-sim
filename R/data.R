45391e6936920513ed42fe27c32f2bae2230d1a9
Mark Jones <mark.jones1@sydney.edu.au>
2024-07-26 15:52:03 +0800

    Working version with adj for surgery trt

--- R/data.R
+++ R/data.R
@@ -2,12 +2,12 @@ source("./R/init.R")
 
 
 
-g_silo = c("e", "l" , "c") 
-g_pr_silo <- c(0.3, 0.5, 0.2)
+g_silo = c("e", "c", "l") 
+g_pr_silo <- c(0.3, 0.2, 0.5)
 names(g_pr_silo) <- g_silo
 
 g_pr_jnt <- matrix(
-  c(0.4, 0.6, 0.7, 0.3, 0.5, 0.5), 3, 2, byrow = T
+  c(0.4, 0.6, 0.5, 0.5, 0.7, 0.3), 3, 2, byrow = T
 )
 colnames(g_pr_jnt) <- c("knee", "hip")
 rownames(g_pr_jnt) <- g_silo
@@ -37,20 +37,17 @@ N = 1e5
 mu = 0
 # all effects are sum to zero
 # silo (e, l, c)
-b_silo = c(0.4, 0.1, -0.5)
-b_silo = c(qlogis(0.7), qlogis(0.6), qlogis(0.6))
+b_silo = c(0.5, -0.3, -0.2)
 # jnt (knee, hip)
 b_jnt = c(-0.4, 0.4)
 # domains:
 # surgery 
 
 b_pref = c(0.2, -0.2)
-# non-rand: dair, one, two
-# rand: dair, rev (one), rev (two)
-# b_d1 = c(-1, -0.4, 0.4, 0.2, 0.3, 0.5)
-b_d1 = c(0, 0, 0, -1, 0, 1)
-# non-rand dair, not-rand rev, dair, rev
-# b_d1 = c(-1, -0.1, 0.4, 0.7)
+# non rand surg
+b_g1 = c(0, 0, 0)
+# rand surg
+b_d1 = c(-1, 0, 1)
 # ab backbone duration 
 # non-rand (received dair), non-rand (received one), non-rand (received two),
 # 6wk, 12wk
@@ -68,12 +65,8 @@ get_sim_data <- function(
     
     # all effects are sum to zero
     
-    # silo (e, l, c)
-    b_silo = c(0.4, 0.1, -0.5),
-    
-    # b_silo is ref log-odds for each silo
-    # b_silo = c(qlogis(0.7), qlogis(0.6), qlogis(0.6)),
-    
+    # silo (e, c, l)
+    b_silo = c(0.5, -0.3, -0.2),
     
     # jnt (knee, hip)
     b_jnt = c(-0.4, 0.4),
@@ -85,7 +78,8 @@ get_sim_data <- function(
     b_pref = c(0.2, -0.2),
     # non-rand dair, non-rand rev(one), non-rand rev(two), 
     # rand dair, rand rev(one), rand rev(two), 
-    b_d1 = c(0, 0, 0,  -1, 0, 1),
+    b_g1 = c(0, 0, 0),
+    b_d1 = c(-1, 0, 1),
     # ab backbone duration 
     # non-rand (received dair), non-rand (received one), non-rand (received two),
     # 6wk, 12wk
@@ -98,6 +92,7 @@ get_sim_data <- function(
   
   # stopifnot(all.equal(0, sum(b_silo), tol = sqrt(.Machine$double.eps)))
   stopifnot(all.equal(0, sum(b_jnt), tol = sqrt(.Machine$double.eps)))
+  stopifnot(all.equal(0, sum(b_g1), tol = sqrt(.Machine$double.eps)))
   stopifnot(all.equal(0, sum(b_d1), tol = sqrt(.Machine$double.eps)))
   stopifnot(all.equal(0, sum(b_d2), tol = sqrt(.Machine$double.eps)))
   stopifnot(all.equal(0, sum(b_d3), tol = sqrt(.Machine$double.eps)))
@@ -107,6 +102,7 @@ get_sim_data <- function(
   K_s <- length(b_silo)
   K_j <- length(b_jnt)
   K_p <- length(b_pref)
+  K_g1 <- length(b_g1)
   K_d1 <- length(b_d1)
   K_d2 <- length(b_d2)
   K_d3 <- length(b_d3)
@@ -119,6 +115,7 @@ get_sim_data <- function(
   d[, silo := sample(1:K_s, size = N, replace = T, prob = g_pr_silo)]
   d[silo == 1, jnt := sample(1:K_j, size = .N, replace = T, prob = g_pr_jnt[1, ])]
   d[silo == 2, jnt := sample(1:K_j, size = .N, replace = T, prob = g_pr_jnt[2, ])]
+  # silo 3 is the late acute
   d[silo == 3, jnt := sample(1:K_j, size = .N, replace = T, prob = g_pr_jnt[3, ])]
   
   # Domains
@@ -126,47 +123,36 @@ get_sim_data <- function(
   # Surgery
   # early - non-randomised
   
-  # pref for one-stage or two stage if forced to have revision
-  d[silo == 1, pref := 99]
-  # selected surgical assignment dair vs rev, which is basically just taking 
-  # the preference probabilities and aggregating them appropriately
-  d[silo == 1, d1 := sample(1:3, size = .N, replace = T, prob = g_pr_e_pref[1,])]
-  # selection surgical type dair (1), one(2), two(3)
-  # dair is fixed
-  # d[silo == 1 & d1 == -98, d1 := 1]
-  # # under revision, d1 is now preference, i.e. preference influences assignment.
-  # d[silo == 1 & d1 == -99, d1 := pref + 1]
-  
-  # late - randomised
-  # pref for one-stage or two stage if forced to have revision
-  d[silo == 2, pref := sample(1:2, size = .N, replace = T, prob = g_pr_l_pref[2, 2:3])]
-  # 1:1 rand assignment to dair (3) or rev (4) 
-  d[silo == 2, d1 := sample(c(-98, -99), size = .N, replace = T)]
-  # selection surgical type dair (1), one(2), two(3)
-  # dair is fixed
-  d[silo == 2 & d1 == -98, d1 := 4]
+  # preference is collinear with treatment selection
+  d[silo == 1, pref_rev := sample(1:2, size = .N, replace = T, prob = g_pr_e_pref[2, 2:3])]
+  d[silo == 1, g1 := sample(c(-98, -99), size = .N, replace = T, prob = g_pr_e_surg)]
+  d[silo == 1 & g1 == -98, g1 := 1]
   # under revision, d1 has high probability of being the preference
-  d[silo == 2 & d1 == -99 & pref == 1, d1 := sample(5:6, .N, replace = T, prob = c(0.95, 0.05))]
-  d[silo == 2 & d1 == -99 & pref == 2, d1 := sample(5:6, .N, replace = T, prob = c(0.05, 0.95))]
+  d[silo == 1 & g1 == -99 & pref_rev == 1, g1 := sample(2:3, .N, replace = T, prob = c(0.95, 0.05))]
+  d[silo == 1 & g1 == -99 & pref_rev == 2, g1 := sample(2:3, .N, replace = T, prob = c(0.05, 0.95))]
+
+  d[silo == 2, pref_rev := sample(1:2, size = .N, replace = T, prob = g_pr_c_pref[2, 2:3])]
+  d[silo == 2, g1 := sample(c(-98, -99), size = .N, replace = T, prob = g_pr_c_surg)]
+  d[silo == 2 & g1 == -98, g1 := 1]
+  # under revision, d1 has high probability of being the preference
+  d[silo == 2 & g1 == -99 & pref_rev == 1, g1 := sample(2:3, .N, replace = T, prob = c(0.95, 0.05))]
+  d[silo == 2 & g1 == -99 & pref_rev == 2, g1 := sample(2:3, .N, replace = T, prob = c(0.05, 0.95))]
   
-  # chronic
-  # pref for one-stage or two stage if forced to have revision
-  # d[silo == 3, pref := sample(1:2, size = .N, replace = T, prob = g_pr_c_pref[2, 2:3])]
-  d[silo == 3, pref := 99]
-  # selected surgical assignment dair vs rev (not-rand dair (1), not-rand rev (2))
-  # d[silo == 3, d1 := sample(c(-98, -99), size = .N, replace = T, prob = g_pr_c_surg)]
-  d[silo == 3, d1 := sample(1:3, size = .N, replace = T, prob = g_pr_c_pref[1,])]
+  
+  # late acute is a randomised assignment for surgical domain
+  d[silo == 3, pref_rev := sample(1:2, size = .N, replace = T, prob = g_pr_l_pref[2, 2:3])]
+  # 1:1 rand assignment to dair (3) or rev (4) 
+  d[silo == 3, d1 := sample(c(-98, -99), size = .N, replace = T)]
   # selection surgical type dair (1), one(2), two(3)
   # dair is fixed
-  # d[silo == 3 & d1 == -98, d1 := 1]
-  # # under revision, d1 is preference
-  # d[silo == 3 & d1 == -99, d1 := pref + 1]
-  
+  d[silo == 3 & d1 == -98, d1 := 1]
+  # under revision, d1 has high probability of being the preference
+  d[silo == 3 & d1 == -99 & pref_rev == 1, d1 := sample(2:3, .N, replace = T, prob = c(0.95, 0.05))]
+  d[silo == 3 & d1 == -99 & pref_rev == 2, d1 := sample(2:3, .N, replace = T, prob = c(0.05, 0.95))]
+
   # Outcome
-  # d[, eta := mu + b_silo[silo] + b_jnt[jnt] + b_pref[pref] + b_d1[d1]]
-  d[silo %in% c(1, 3), eta := mu + b_silo[silo] + b_jnt[jnt] + b_d1[d1] ]
-  d[silo %in% c(2), eta := mu + b_silo[silo] + b_jnt[jnt] + b_pref[pref] + b_d1[d1] ]
-  # d[, eta := mu + b_silo[silo] + b_jnt[jnt] + b_pref[pref] + b_d1[d1] ]
+  d[silo %in% 1:2, eta := mu + b_silo[silo] + b_jnt[jnt] + b_pref[pref_rev] + b_g1[g1] ]
+  d[silo %in% 3, eta := mu + b_silo[silo] + b_jnt[jnt] + b_pref[pref_rev] + b_d1[d1] ]
   d[, y := rbinom(N, 1, plogis(eta))]
 
   # Convert design to lower dim
@@ -174,6 +160,7 @@ get_sim_data <- function(
   X_s <- diag(K_s)
   X_j <- diag(K_j)
   X_p <- diag(K_p)
+  X_g1 <- diag(K_g1)
   X_d1 <- diag(K_d1)
   X_d2 <- diag(K_d2)
   X_d3 <- diag(K_d3)
@@ -183,6 +170,7 @@ get_sim_data <- function(
   S_s <- X_s - (1 / K_s )
   S_j <- X_j - (1 / K_j )
   S_p <- X_p - (1 / K_p )
+  S_g1 <- X_g1 - (1 / K_g1 )
   S_d1 <- X_d1 - (1 / K_d1 )
   S_d2 <- X_d2 - (1 / K_d2 )
   S_d3 <- X_d3 - (1 / K_d3 )
@@ -192,6 +180,7 @@ get_sim_data <- function(
   Q_s <- eigen(S_s)$vector[, 1:(K_s - 1)]
   Q_j <- eigen(S_j)$vector[, 1:(K_j - 1)]
   Q_p <- eigen(S_p)$vector[, 1:(K_p - 1)]
+  Q_g1 <- eigen(S_g1)$vector[, 1:(K_g1 - 1)]
   Q_d1 <- eigen(S_d1)$vector[, 1:(K_d1 - 1)]
   Q_d2 <- eigen(S_d2)$vector[, 1:(K_d2 - 1)]
   Q_d3 <- eigen(S_d3)$vector[, 1:(K_d3 - 1)]
@@ -201,6 +190,7 @@ get_sim_data <- function(
   b_s_s <- t(Q_s) %*% b_silo
   b_j_s <- t(Q_j) %*% b_jnt
   b_p_s <- t(Q_p) %*% b_pref
+  b_g1_s <- t(Q_g1) %*% b_g1
   b_d1_s <- t(Q_d1) %*% b_d1
   b_d2_s <- t(Q_d2) %*% b_d2
   b_d3_s <- t(Q_d3) %*% b_d3
@@ -210,28 +200,29 @@ get_sim_data <- function(
   X_s_s <- X_s %*% Q_s
   X_j_s <- X_j %*% Q_j
   X_p_s <- X_p %*% Q_p
+  X_g1_s <- X_g1 %*% Q_g1
   X_d1_s <- X_d1 %*% Q_d1
   X_d2_s <- X_d2 %*% Q_d2
   X_d3_s <- X_d3 %*% Q_d3
   X_d4_s <- X_d4 %*% Q_d4
   
   
-  d_grid <- CJ(silo = 1:K_s, jnt = 1:K_j, p = 1:K_p, d1 = 1:K_d1 )
-  # irrelevant combinations
-  # no randomisation within surgical domain
-  d_grid <- d_grid[! (silo == 1 & d1 %in% 4:6)]
-  d_grid <- d_grid[! (silo == 3 & d1 %in% 4:6)]
-  
-  X_full_s <- matrix(NA, nrow(d_grid), 9)
-  for(i in 1:nrow(d_grid)){
-    
-    X_full_s[i, ] <- c(
-      X_s_s[d_grid[i, silo], ],
-      X_j_s[d_grid[i, jnt], ],
-      X_p_s[d_grid[i, p], ],
-      X_d1_s[d_grid[i, d1], ]
-      )
-  }
+  # d_grid <- CJ(silo = 1:K_s, jnt = 1:K_j, d1 = 1:K_d1 )
+  # # irrelevant combinations
+  # # no randomisation within surgical domain
+  # # d_grid <- d_grid[! (silo == 1 & d1 %in% 4:6)]
+  # # d_grid <- d_grid[! (silo == 3 & d1 %in% 4:6)]
+  # 
+  # X_full_s <- matrix(NA, nrow(d_grid), 4)
+  # for(i in 1:nrow(d_grid)){
+  #   
+  #   X_full_s[i, ] <- c(
+  #     X_s_s[d_grid[i, silo], ],
+  #     X_j_s[d_grid[i, jnt], ],
+  #     # X_p_s[d_grid[i, p], ],
+  #     X_d1_s[d_grid[i, d1], ]
+  #     )
+  # }
   
   
   list(
@@ -239,15 +230,19 @@ get_sim_data <- function(
     
     mu = mu, 
     b_silo = b_silo, b_jnt = b_jnt, b_pref = b_pref, 
+    b_g1 = b_g1, 
     b_d1 = b_d1, b_d2 = b_d2, b_d3 = b_d3, b_d4 = b_d4, 
     
     K_s = K_s, K_j = K_j, K_p  = K_p,
+    K_g1 = K_g1,
     K_d1 = K_d1, K_d2 = K_d2 , K_d3 = K_d3, K_d4 = K_d4,
     
     X_s_s  = X_s_s, X_j_s  = X_j_s, X_p_s  = X_p_s, 
+    X_g1_s = X_g1_s, 
     X_d1_s = X_d1_s, X_d2_s = X_d2_s,
-    X_d3_s = X_d3_s, X_d4_s = X_d4_s, 
-    X_full_s = X_full_s
+    X_d3_s = X_d3_s, X_d4_s = X_d4_s
+    # , 
+    # X_full_s = X_full_s
   )
   
 }
@@ -258,32 +253,42 @@ main <- function(){
   m1 <- cmdstanr::cmdstan_model("stan/model09.stan")
   ll <- get_sim_data(
     N = 1e6, 
-    b_d1 = c(-1, 1, 0,  -1, 0, 1))
-  d_mod <- ll$d[, .(y = sum(y), n = .N), keyby = .(silo, jnt, pref, d1)]
-  d_mod[, p_obs := y / n]
-  d_mod[, eta_obs := qlogis(p_obs)]
+    b_g1 = c(-0.25, 0.25, 0),
+    b_d1 = c(-1, 0, 1)
+    )
   
+  d_mod1 <- ll$d[silo %in% 1:2, .(y = sum(y), n = .N), keyby = .(silo, jnt, pref_rev, g1)]
+  d_mod1[, p_obs := y / n]
+  d_mod1[, eta_obs := qlogis(p_obs)]
+  d_mod1
   
-  dtmp <- ll$d[, .(y = sum(y), n = .N), keyby = .(silo, d1)]
-  dtmp[, p_obs := y / n]
-  dtmp[, eta_obs := qlogis(p_obs)]
-  dtmp
-
+  d_mod2 <- ll$d[silo %in% 3, .(y = sum(y), n = .N), keyby = .(silo, jnt, pref_rev, d1)]
+  d_mod2[, p_obs := y / n]
+  d_mod2[, eta_obs := qlogis(p_obs)]
+  d_mod2
+  
   ld <- list(
-    N = nrow(d_mod), 
-    N1 = nrow(d_mod[silo == 1]), 
-    N2 = nrow(d_mod[silo == 2]),
-    N3 = nrow(d_mod[silo == 3]),
-    ix1 = d_mod[silo == 1, which = T], 
-    ix2 = d_mod[silo == 2, which = T], 
-    ix3 = d_mod[silo == 3, which = T], 
+    N = nrow(d_mod1) + nrow(d_mod2), 
+    N1 = nrow(d_mod1[silo == 1]), 
+    N2 = nrow(d_mod1[silo == 2]),
+    N3 = nrow(d_mod2),
     
-    y = d_mod$y, 
-    n = d_mod$n, 
-    silo = d_mod$silo, 
-    jnt = d_mod$jnt,
-    pref = d_mod$pref,
-    d1 = d_mod$d1,
+    ixs1 = d_mod1[silo == 1, which = T], 
+    ixs2 = d_mod1[silo == 2, which = T], 
+    ixs3 = d_mod2[silo == 3, which = T],  
+    
+    y = c(d_mod1[, y], d_mod2[, y]), 
+    n = c(d_mod1[, n], d_mod2[, n]), 
+    
+    silo_1 = d_mod1[, silo], 
+    jnt_1 = d_mod1[, jnt],
+    pref_1 = d_mod1[, pref_rev],
+    g1_1 = d_mod1[, g1],
+    
+    silo_2 = d_mod2[, silo], 
+    jnt_2 = d_mod2[, jnt], 
+    pref_2 = d_mod2[, pref_rev],
+    d1_2 = d_mod2[, d1],
     
     nrXs = nrow(ll$X_s_s), ncXs = ncol(ll$X_s_s),
     Xsdes = ll$X_s_s, ss = rep(1, ncol(ll$X_s_s)),
@@ -293,6 +298,9 @@ main <- function(){
     
     nrXp = nrow(ll$X_p_s), ncXp = ncol(ll$X_p_s), 
     Xpdes = ll$X_p_s, sp = rep(1, ncol(ll$X_p_s)),
+    
+    nrXg1 = nrow(ll$X_g1_s), ncXg1 = ncol(ll$X_g1_s), 
+    Xg1des = ll$X_d1_s, sg1 = rep(1, ncol(ll$X_g1_s)),
     
     nrXd1 = nrow(ll$X_d1_s), ncXd1 = ncol(ll$X_d1_s), 
     Xd1des = ll$X_d1_s, sd1 = rep(1, ncol(ll$X_d1_s)),
@@ -328,12 +336,19 @@ main <- function(){
     par = ll$b_jnt,
     post_mu = colMeans(post_mu)
   )
-  # 
-  # #
+  #
   m_post_s <- f1$draws(variables = c("bp"), format = "matrix")
   post_mu <- m_post_s %*% t(cbind(ll$X_p_s))
   rbind(
     par = ll$b_pref,
+    post_mu = colMeans(post_mu)
+  )
+  
+  # 
+  m_post_s <- f1$draws(variables = c("bg1"), format = "matrix")
+  post_mu <- m_post_s %*% t(cbind(ll$X_d1_s))
+  rbind(
+    par = ll$b_g1,
     post_mu = colMeans(post_mu)
   )
   
@@ -344,7 +359,9 @@ main <- function(){
     par = ll$b_d1,
     post_mu = colMeans(post_mu)
   )
-  # 
+  
+  
+  
 }
 
 
@@ -354,10 +371,10 @@ sim <- function(){
   
   m1 <- cmdstanr::cmdstan_model("stan/model09.stan")
   
-  mu = 0
+  mu = 1
   # all effects are sum to zero
   # silo (e, l, c)
-  b_silo = c(0.4, 0.1, -0.5)
+  b_silo = c(0.5, -0.3, -0.2)
   # jnt (knee, hip)
   b_jnt = c(-0.4, 0.4)
   # surgery 
@@ -366,7 +383,8 @@ sim <- function(){
   b_pref = c(0.2, -0.2)
   # non-rand dair, non-rand rev(one), non-rand rev(two), 
   # rand dair, rand rev(one), rand rev(two), 
-  b_d1 = c(0,  -1, 0,  0, 0, 1)
+  b_g1 = c(-(1/3), -(1/3), 2/3)
+  b_d1 = c(-1, 0, 1)
   # ab backbone duration 
   # non-rand (received dair), non-rand (received one), non-rand (received two),
   # 6wk, 12wk
@@ -381,25 +399,34 @@ sim <- function(){
       
       ll <- get_sim_data(
         N = 2.5e3, 
-        b_d1 = b_d1)
-      d_mod <- ll$d[, .(y = sum(y), n = .N), keyby = .(silo, jnt, pref, d1)]
+        mu = mu, b_silo = b_silo, b_jnt = b_jnt, b_pref = b_pref,
+        b_g1 = b_g1, b_d1 = b_d1)
       
+      d_mod1 <- ll$d[silo %in% 1:2, .(y = sum(y), n = .N), keyby = .(silo, jnt, pref_rev, g1)]
+      d_mod2 <- ll$d[silo %in% 3, .(y = sum(y), n = .N), keyby = .(silo, jnt, pref_rev, d1)]
       
       ld <- list(
-        N = nrow(d_mod), 
-        N1 = nrow(d_mod[silo == 1]), 
-        N2 = nrow(d_mod[silo == 2]),
-        N3 = nrow(d_mod[silo == 3]),
-        ix1 = d_mod[silo == 1, which = T], 
-        ix2 = d_mod[silo == 2, which = T], 
-        ix3 = d_mod[silo == 3, which = T], 
+        N = nrow(d_mod1) + nrow(d_mod2), 
+        N1 = nrow(d_mod1[silo == 1]), 
+        N2 = nrow(d_mod1[silo == 2]),
+        N3 = nrow(d_mod2),
         
-        y = d_mod$y, 
-        n = d_mod$n, 
-        silo = d_mod$silo, 
-        jnt = d_mod$jnt,
-        pref = d_mod$pref,
-        d1 = d_mod$d1,
+        ixs1 = d_mod1[silo == 1, which = T], 
+        ixs2 = d_mod1[silo == 2, which = T], 
+        ixs3 = d_mod2[silo == 3, which = T],  
+        
+        y = c(d_mod1[, y], d_mod2[, y]), 
+        n = c(d_mod1[, n], d_mod2[, n]), 
+        
+        silo_1 = d_mod1[, silo], 
+        jnt_1 = d_mod1[, jnt],
+        pref_1 = d_mod1[, pref_rev],
+        g1_1 = d_mod1[, g1],
+        
+        silo_2 = d_mod2[, silo], 
+        jnt_2 = d_mod2[, jnt], 
+        pref_2 = d_mod2[, pref_rev],
+        d1_2 = d_mod2[, d1],
         
         nrXs = nrow(ll$X_s_s), ncXs = ncol(ll$X_s_s),
         Xsdes = ll$X_s_s, ss = rep(1, ncol(ll$X_s_s)),
@@ -410,16 +437,14 @@ sim <- function(){
         nrXp = nrow(ll$X_p_s), ncXp = ncol(ll$X_p_s), 
         Xpdes = ll$X_p_s, sp = rep(1, ncol(ll$X_p_s)),
         
+        nrXg1 = nrow(ll$X_g1_s), ncXg1 = ncol(ll$X_g1_s), 
+        Xg1des = ll$X_d1_s, sg1 = rep(1, ncol(ll$X_g1_s)),
+        
         nrXd1 = nrow(ll$X_d1_s), ncXd1 = ncol(ll$X_d1_s), 
         Xd1des = ll$X_d1_s, sd1 = rep(1, ncol(ll$X_d1_s)),
         
         prior_only = 0
       )
-      
-      # f1 <- m1$sample(
-      #   ld, iter_warmup = 1000, iter_sampling = 1000,
-      #   parallel_chains = 2, chains = 2, refresh = 0, show_exceptions = F,
-      #   max_treedepth = 13)
       
       snk <- capture.output(
         f1 <- m1$pathfinder(ld, num_paths=20, single_path_draws=200,
@@ -444,17 +469,24 @@ sim <- function(){
       m_post_s <- f1$draws(variables = c("bp"), format = "matrix")
       post_mu <- m_post_s %*% t(cbind(ll$X_p_s))
       v_out <- c(v_out,  colMeans(post_mu))
-      
+       
       #
       m_post_s <- f1$draws(variables = c("bd1"), format = "matrix")
       post_mu <- m_post_s %*% t(cbind(ll$X_d1_s))
+      v_out <- c(v_out,  colMeans(post_mu))
+      
+      #
+      m_post_s <- f1$draws(variables = c("bg1"), format = "matrix")
+      post_mu <- m_post_s %*% t(cbind(ll$X_g1_s))
       v_out <- c(v_out,  colMeans(post_mu))
       
       names(v_out) <- c("mu", 
                         paste0("bs",seq_along(ll$b_silo)),
                         paste0("bj",seq_along(ll$b_jnt)),
                         paste0("bp",seq_along(ll$b_pref)),
-                        paste0("bd1",seq_along(ll$b_d1)))
+                        paste0("bd1",seq_along(ll$b_d1)),
+                        paste0("bg1",seq_along(ll$b_g1))
+                        )
       
       v_out
       
@@ -465,27 +497,34 @@ sim <- function(){
                                                   paste0("bs",seq_along(b_silo)),
                                                   paste0("bj",seq_along(b_jnt)),
                                                   paste0("bp",seq_along(b_pref)),
-                                                  paste0("bd1",seq_along(b_d1))))]
+                                                  paste0("bd1",seq_along(b_d1)),
+                                                  paste0("bg1",seq_along(b_g1))
+                                                  )
+                             )
+        ]
   
   d_tru <- data.table(
     variable = c("mu", 
                  paste0("bs",seq_along(b_silo)),
                  paste0("bj",seq_along(b_jnt)),
                  paste0("bp",seq_along(b_pref)),
-                 paste0("bd1",seq_along(b_d1))),
+                 paste0("bd1",seq_along(b_d1)),
+                 paste0("bg1",seq_along(b_g1))),
     value = c(
       mu,
       b_silo,
       b_jnt,
       b_pref,
-      b_d1
+      b_d1,
+      b_g1
     )
   )
   d_tru[, variable := factor(variable, levels = c("mu", 
                                                   paste0("bs",seq_along(b_silo)),
                                                   paste0("bj",seq_along(b_jnt)),
                                                   paste0("bp",seq_along(b_pref)),
-                                                  paste0("bd1",seq_along(b_d1))))]
+                                                  paste0("bd1",seq_along(b_d1)),
+                                                  paste0("bg1",seq_along(b_g1))))]
   
   ggplot(d_res, aes(x = value, group = variable)) +
     geom_density() +
--- stan/model09.stan
+++ stan/model09.stan
@@ -2,23 +2,26 @@
 // instructions:
 data{ 
   int N;
-  array[N] int y;
-  array[N] int n;
-  
-  // int S;
-  array[N] int silo;
-  
   int N1;
   int N2;
   int N3;
-  array[N1] int ix1;
-  array[N2] int ix2;
-  array[N3] int ix3;
+  array[N1] int ixs1;
+  array[N2] int ixs2;
+  array[N3] int ixs3;
   
-  array[N] int jnt;
-  array[N] int pref;
-  // arm index domain 1
-  array[N] int d1;
+  array[N] int y;
+  array[N] int n;
+  
+  array[N1+N2] int silo_1;
+  array[N1+N2] int jnt_1;
+  array[N1+N2] int pref_1;
+  array[N1+N2] int g1_1;
+  
+  array[N3] int silo_2;
+  array[N3] int jnt_2;
+  array[N3] int pref_2;
+  array[N3] int d1_2;
+
   // design matrices
   // silo
   int nrXs;
@@ -30,49 +33,55 @@ data{
   int ncXj;
   matrix[nrXj, ncXj] Xjdes;
   vector[ncXj] sj;
-  // surgical preference
+  // prev rev
   int nrXp;
   int ncXp;
   matrix[nrXp, ncXp] Xpdes;
   vector[ncXp] sp;
-  // domain 1 surgery
+  // domain 1 (non-randomised surgery)
+  int nrXg1;
+  int ncXg1;
+  matrix[nrXg1, ncXg1] Xg1des;
+  vector[ncXg1] sg1;
+  // domain 1 randomised surgery (rev portions selected)
   int nrXd1;
   int ncXd1;
   matrix[nrXd1, ncXd1] Xd1des;
   vector[ncXd1] sd1;
-  
-  
+ 
   int prior_only;
 }
 transformed data{
   
   // build full design matrices
-  matrix[N1, ncXs] X1s = Xsdes[silo[ix1]];
-  matrix[N1, ncXj] X1j = Xjdes[jnt[ix1]];
-  matrix[N1, ncXd1] X1d1 = Xd1des[d1[ix1]];
+  matrix[N1, ncXs] X1s = Xsdes[silo_1[ixs1]];
+  matrix[N1, ncXj] X1j = Xjdes[jnt_1[ixs1]];
+  matrix[N1, ncXj] X1p = Xjdes[pref_1[ixs1]];
+  matrix[N1, ncXg1] X1g1 = Xg1des[g1_1[ixs1]];
   
-  matrix[N2, ncXs] X2s = Xsdes[silo[ix2]];
-  matrix[N2, ncXj] X2j = Xjdes[jnt[ix2]];
-  matrix[N2, ncXp] X2p = Xpdes[pref[ix2]];
-  matrix[N2, ncXd1] X2d1 = Xd1des[d1[ix2]];
+  matrix[N2, ncXs] X2s = Xsdes[silo_1[ixs2]];
+  matrix[N2, ncXj] X2j = Xjdes[jnt_1[ixs2]];
+  matrix[N2, ncXj] X2p = Xjdes[pref_1[ixs2]];
+  matrix[N2, ncXg1] X2g1 = Xg1des[g1_1[ixs2]];
   
-  matrix[N3, ncXs] X3s = Xsdes[silo[ix3]];
-  matrix[N3, ncXj] X3j = Xjdes[jnt[ix3]];
-  matrix[N3, ncXd1] X3d1 = Xd1des[d1[ix3]];
-  
+  matrix[N3, ncXs] X3s = Xsdes[silo_2[ixs3]];
+  matrix[N3, ncXj] X3j = Xjdes[jnt_2[ixs3]];
+  matrix[N3, ncXj] X3p = Xjdes[pref_2[ixs3]];
+  matrix[N3, ncXd1] X3d1 = Xd1des[d1_2[ixs3]];
+
 }
 parameters{
   real mu;
   vector[ncXs] bs;
   vector[ncXj] bj;
   vector[ncXp] bp;
+  vector[ncXg1] bg1;
   vector[ncXd1] bd1;
 }
 transformed parameters{
-  // vector[N] eta = mu + Xs*bs + Xj*bj + Xp*bp + Xd1*bd1;
-  vector[N1] eta1 = mu + X1s*bs + X1j*bj + X1d1*bd1;
-  vector[N2] eta2 = mu + X2s*bs + X2j*bj + X2p*bp + X2d1*bd1;
-  vector[N3] eta3 = mu + X3s*bs + X3j*bj + X3d1*bd1;
+  vector[N1] eta1 = mu + X1s*bs + X1j*bj + X1p*bp + X1g1*bg1;
+  vector[N2] eta2 = mu + X2s*bs + X2j*bj + X2p*bp + X2g1*bg1;
+  vector[N3] eta3 = mu + X3s*bs + X3j*bj + X3p*bp + X3d1*bd1;
   vector[N] eta;
   eta[1:N1] = eta1;
   eta[(N1+1):(N1+N2)] = eta2;
@@ -84,6 +93,7 @@ model{
   target += normal_lpdf(bs | 0, ss);
   target += normal_lpdf(bj | 0, sj);
   target += normal_lpdf(bp | 0, sp);
+  target += normal_lpdf(bg1 | 0, sg1);
   target += normal_lpdf(bd1 | 0, sd1);
   
   if(!prior_only){
